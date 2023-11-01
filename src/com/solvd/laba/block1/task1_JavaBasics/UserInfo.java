3066a17328703843807f08e88f5147b207a64fbe
JuliiaRak <juliarak@ukr.net>
2023-11-01 19:03:52 +0200

    initial commit

---
+++ src/com/solvd/laba/block1/task1_JavaBasics/UserInfo.java
@@ -0,0 +1,13 @@
+package com.solvd.laba.block1.task1_JavaBasics;
+
+public class UserInfo {
+    public static void main(String[] args) {
+        if (args.length >= 2) {
+            String name = args[0];
+            String surname = args[1];
+            System.out.println("Hello! Nice to see you " + name + " " + surname + "!");
+        } else {
+            System.out.println("Please provide your name and surname as command line arguments.");
+        }
+    }
+}
